82fb7e67dd1e168fc1e56c0ecedb00afc0527564
tarun-mishra-1897 <mishratarun997@gmail.com>
2025-05-14 16:32:01 +0530

    Add files via upload

---
+++ statistics assignment.py
@@ -0,0 +1,116 @@
+#1. What is a random variable in probability theory?
+
+#ans.>> A set of possible value from a random experiment .t maps outcomes of a probability space to real numbers.
+
+#2. What are the types of random variables?
+
+#ans.>> there are two types of random variable :
+#      1. discrete random variable : it takes count values (0,1,2 etc)
+#      2.continuous random varibale : it takes any value with in range...
+
+#3. Difference between discrete and continuous distributions:
+
+#ans.>> 1. discrete random variable : it takes count values (0,1,2 etc)
+#      2.continuous random varibale : it takes any value with in range
+
+#4. What are probability distribution functions (PDF)?
+
+#ans.>> continuous variable, it takes any value with in a particular range not exact value ,it is called probability distribution functions (PDF)..
+
+#5. Difference between CDF and PDF:
+
+#ans.>> 1.probability distribution functions (PDF) belonges to continuous variable, it takes any value with in a particular range not exact value.
+#      2. cummulative distribution function (CDF),Gives the probability that the variable is less than or equal to a specific value.
+
+#6. What is a discrete uniform distribution?
+
+#ans.>> in a discrete uniform distribution , the outcome are discreate and having some probability like.. tossing a coin , rolling a dice..
+
+#7. Key properties of a Bernoulli distribution:
+
+#ans.>> these are properties of bernoulli distribution :
+#      1.number of trail should be finite
+#      2.each trail should be independent
+#      3.there are 2 possible outcome(pass,fail)
+#      4.possiblity of each outcome in each trail should be same..
+
+#8. What is the binomial distribution and its use?
+
+#ans.>> binomial distribution is the 'n' bernoulli distribution (where 'n' is n no. of success)
+
+#9. What is the Poisson distribution and its applications?
+
+#ans.>>the poission distribution is discrete prob. distribution that describe the no. of event that ouccer within a fixed interval of time or space given a known average rate of occure
+#      it was denoted by lambda(λ)
+#      Example: number of calls at a call center per hour....
+
+#10. What is a continuous uniform distribution?
+
+#ans.>> it is a distribution that has infinite no. of values define in a specified range,All values in a given interval have equal probability density
+#       example : a perfect random no. generator like OTP generator....
+
+#11. Characteristics of a normal distribution:
+
+#ans.>> these are the characterstics of normal distribution :
+#      1. bell shaped distribution
+#      2.symmertrical about mean
+#      3.mean = median = mode
+#      4. skewness = 0
+
+#12. What is the standard normal distribution and why is it important?
+
+#ans.>> standard normal distribution is continuous probability distribution , a special case of normal distribution μ=0,σ=1.
+#      standardized the data and comparing those standardized set of data using Zscore...
+
+#13. What is the Central Limit Theorem (CLT)?
+
+#ans.>> CLT states that the sampling distribution of the sample mean approaches a normal distribution, 
+#       as the sample size increases, regardless of the population's distribution...
+
+#14. How does CLT relate to the normal distribution?
+
+#ans.>>Thanks to CLT, we can use the normal distribution to approximate the behavior of the sample mean for large samples...
+
+#15. Application of Z-statistics in hypothesis testing:
+
+#ans.>>Z-statistics measure how far a sample mean is from the population mean in units of standard deviation. 
+#      Used to test hypotheses about population means.
+
+#16. How to calculate a Z-score and what it represents:
+
+#ans.>> zscore = (x - mean)/standard deviation , it represents x is how many standard deviation far from mean........
+
+#17. Point estimates and interval estimates:
+
+#ans.>> 1.Point estimates : a point estimate is a single value that is used to estimate the true value of a population parameter
+#       2.interval estimates: range of value used to estimate the unknown population parameter,range is used to avoiding uncertainity...
+
+
+#18. Significance of confidence intervals:
+
+#ans.>>interval estimates: range of value used to estimate the unknown population parameter which is lies b/w the such range,
+#      range is used to avoiding uncertainity , it will incerase the level of confidence...
+
+
+#19. Relationship between Z-score and confidence interval:
+
+#ans.>> confidence interval gives a range value with in which mean of population fall , which gives more confidence ......
+
+
+20.# How Z-scores compare different distributions:
+
+#ans.>> it used to standardize values,which make it possible to compare different distributions or units of two data sets....
+
+
+#21. Assumptions for applying the Central Limit Theorem:
+
+#ans.>> 1. the no. of sample should be large
+#       2. the sample size should be greater than  or equal to 30(except the population distribution which is already a normal distribution)
+
+#22. Concept of expected value in a probability distribution:
+
+#ans.>> E(x) =sum(x.p(x))
+
+#23. How probability distribution relates to expected outcome:
+
+#ans.>>The expected value is a weighted average of all possible outcomes, reflecting the long-term average result....
